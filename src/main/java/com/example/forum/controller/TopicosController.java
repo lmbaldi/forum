00e76d69a56a0b2a3e7088d69840704f2d98f2b1
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-03-27 17:59:12 -0300

    add spring-boot-devtools module to avoid restarting server

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -6,15 +6,13 @@ import java.util.List;
 import com.example.forum.modelo.Curso;
 import com.example.forum.modelo.Topico;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    @ResponseBody
     public List<Topico> lista(){
         Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programacao"));
 
