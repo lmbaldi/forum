1191eac922650f49503d5c6568f0fa28f0fe92e4
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-03-27 19:58:15 -0300

    alter TopicosController clas to use TopicoRepository Interface

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -1,22 +1,25 @@
 package com.example.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import com.example.forum.modelo.Curso;
 import com.example.forum.modelo.Topico;
+import com.example.forum.repository.TopicoRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+
     @RequestMapping("/topicos")
     public List<TopicoDto> lista(){
-        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDto.converter(topicos);
     }
     
 }
