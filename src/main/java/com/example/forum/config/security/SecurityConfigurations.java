fda8ad4f3ef073abd3db4dbbb5eea8b40022edf9
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-03-29 20:02:40 -0300

    freeing access to public endpoints

---
+++ src/main/java/com/example/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,35 @@
+package com.example.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+
+    //configuracoes de autenticacao
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }    
+
+    //Configuracoes de autorizacao
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+        .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+    }
+
+    //Configuracoes de recursos estaticos(js, css, images, etc.)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        
+    }
+    
+}
