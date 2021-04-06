1c33929bf75f4558a79cd003c5e30d21b94d967f
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-04-06 14:14:07 -0300

    monitoring api with Spring Boot Admin

--- src/test/java/com/example/forum/ForumApplicationTests.java
+++ src/test/java/com/example/forum/ForumApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.forum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.Assert;
 
 @SpringBootTest
 class ForumApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Assert.assertTrue(true);
 	}
 
 }
