3b6b64a36577a0d29e8ffd3f1a2cf088c9d0dd8f
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-04-06 14:56:16 -0300

    API documentation with SpringFox Swagger

--- src/main/java/com/example/forum/config/security/SecurityConfigurations.java
+++ src/main/java/com/example/forum/config/security/SecurityConfigurations.java
@@ -18,7 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AutenticacaoService autenticacaoService;
@@ -35,37 +35,36 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
         return super.authenticationManager();
     }
 
-    //configuracoes de autenticacao
+    // configuracoes de autenticacao
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
-    }    
+    }
 
-    //Configuracoes de autorizacao
+    // Configuracoes de autorizacao
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-        .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-        .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-        .antMatchers(HttpMethod.GET, "/actuator/*").permitAll()
-        .antMatchers(HttpMethod.POST, "/auth").permitAll()
-        .anyRequest().authenticated()
-        .and().csrf().disable()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and().addFilterBefore(
-            new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class
-            );
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos")
+        .permitAll()
+            .antMatchers(HttpMethod.GET, "/topicos/*")
+            .permitAll().antMatchers(HttpMethod.GET, "/actuator/**")
+            .permitAll().antMatchers(HttpMethod.POST, "/auth")
+            .permitAll().anyRequest().authenticated()
+            .and().csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository),
+                    UsernamePasswordAuthenticationFilter.class);
     }
 
-    //Configuracoes de recursos estaticos(js, css, images, etc.)
+    // Configuracoes de recursos estaticos(js, css, images, etc.)
     @Override
     public void configure(WebSecurity web) throws Exception {
-        
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**",
+                "/swagger-resources/**");
     }
 
-  //  public static void main(String[] args) {
-  //      System.out.println(new BCryptPasswordEncoder().encode("123456"));
-  //  }
+    // public static void main(String[] args) {
+    // System.out.println(new BCryptPasswordEncoder().encode("123456"));
+    // }
 
-    
 }
