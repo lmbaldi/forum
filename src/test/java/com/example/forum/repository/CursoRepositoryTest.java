ed66659330f629f2cb8518b7afeb6e71c6237476
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-04-10 17:56:58 -0300

    Changing the database used during testing

--- src/test/java/com/example/forum/repository/CursoRepositoryTest.java
+++ src/test/java/com/example/forum/repository/CursoRepositoryTest.java
@@ -8,19 +8,32 @@ import com.example.forum.modelo.Curso;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository repository;
 
+    // @Autowired
+    // private TestEntityManager em;
+
     @Test
     void deveriaCarregarUmCursoAoBuscarPeloSeuNome(){
         String nomeCurso = "HTML 5";
-        Curso curso = repository.findByNome(nomeCurso);
 
+        // Curso html5 = new Curso(); 
+        // html5.setNome(nomeCurso);
+        // html5.setCategoria("Programação");
+        // em.persist(html5);
+        
+        Curso curso = repository.findByNome(nomeCurso);
         assertNotNull(curso);
         assertEquals(nomeCurso, curso.getNome());
        
