102fa0cd0325829ca6e946b1a29f7a38d4b81ad9
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-04-10 15:36:13 -0300

    profile-based beans to set up environments

--- src/main/java/com/example/forum/controller/AutenticacaoController.java
+++ src/main/java/com/example/forum/controller/AutenticacaoController.java
@@ -5,6 +5,7 @@ import com.example.forum.controller.dto.TokenDto;
 import com.example.forum.controller.form.LogimForm;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Profile("prod")
 public class AutenticacaoController {
 
     @Autowired
